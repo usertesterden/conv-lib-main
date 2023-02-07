e70779487e1779164c3e8f851708182160654ddf
5imba <bogllev3@gmail.com>
2023-02-07 19:55:03 +0200

    Provide tag creation

--- secret-lib/src/main/java/com/battlecreek/secret_lib/utils/SecretKeeper.kt
+++ secret-lib/src/main/java/com/battlecreek/secret_lib/utils/SecretKeeper.kt
@@ -3,16 +3,17 @@ package com.battlecreek.secret_lib.utils
 import android.net.Uri
 import java.util.TimeZone
 
-class SecretKeeper(
-    host: String,
-    path: String,
-    private val keys: List<String>,
-) {
+class SecretKeeper {
 
     private val keeper = Uri.Builder()
-    private val root: String
+    private var root: String = ""
+    private var keys: List<String>? = null
 
-    init {
+    fun init(
+        host: String,
+        path: String,
+        keys: List<String>,
+    ) {
         keeper.scheme("https")
         keeper.authority(host)
         root = keeper.build().toString() + "/"
@@ -23,13 +24,26 @@ class SecretKeeper(
         return root
     }
 
-    fun getSecret(deep: String, gadId: String): String {
+    fun getSecret(pursuitResult: String, twist: String): String {
         keeper.clearQuery()
-        keeper.appendQueryParameter(keys[0], keys[1])
-        keeper.appendQueryParameter(keys[2], deep)
-        keeper.appendQueryParameter(keys[3], deep)
-        keeper.appendQueryParameter(keys[4], gadId)
-        keeper.appendQueryParameter(keys[5], TimeZone.getDefault().id)
+        keeper.appendQueryParameter(keys?.get(0).toString(), keys?.get(1).toString())
+        keeper.appendQueryParameter(keys?.get(2).toString(), pursuitResult)
+        keeper.appendQueryParameter(keys?.get(3).toString(), pursuitResult)
+        keeper.appendQueryParameter(keys?.get(4).toString(), twist)
+        keeper.appendQueryParameter(keys?.get(5).toString(), TimeZone.getDefault().id)
         return keeper.build().toString()
+    }
+
+    fun provideAttention(pursuitResult: String): String {
+        return when {
+            pursuitResult.isEmpty() || pursuitResult == "null" -> {
+                "organic"
+            }
+            else -> {
+                pursuitResult
+                    .replace("myapp://", "")
+                    .substringBefore("/")
+            }
+        }
     }
 }
