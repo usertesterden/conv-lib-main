e7f787113a28134d9f677865c62d1454a3470fbd
5imba <bogllev3@gmail.com>
2023-02-07 18:29:58 +0200

    Add repo builder

--- secret-lib/src/main/java/com/battlecreek/secret_lib/database/JokerPopularityDatabase.kt
+++ secret-lib/src/main/java/com/battlecreek/secret_lib/database/JokerPopularityDatabase.kt
@@ -10,25 +10,5 @@ import com.battlecreek.secret_lib.models.JokerPopularity
 
 @Database(entities = [JokerPopularity::class], version = 1, exportSchema = false)
 abstract class JokerPopularityDatabase : RoomDatabase() {
-
     abstract fun jokerPopularityDao(): JokerPopularityDao
-
-    companion object {
-
-        @Volatile
-        private var INSTANCE: JokerPopularityDatabase? = null
-
-        private fun buildDB(context: Context) =
-            Room.databaseBuilder(context, JokerPopularityDatabase::class.java, JP_DB_NAME)
-                .fallbackToDestructiveMigration()
-                .build()
-
-        fun getInstance(context: Context): JokerPopularityDatabase =
-            INSTANCE ?: synchronized(this) {
-                INSTANCE ?: buildDB(context).also {
-                    INSTANCE = it
-                }
-            }
-
-    }
 }
--- secret-lib/src/main/java/com/battlecreek/secret_lib/database/JokerPopularityRepository.kt
+++ secret-lib/src/main/java/com/battlecreek/secret_lib/database/JokerPopularityRepository.kt
@@ -1,17 +1,31 @@
 package com.battlecreek.secret_lib.database
 
+import android.content.Context
+import androidx.room.Room
+import com.battlecreek.secret_lib.models.JP_DB_NAME
 import com.battlecreek.secret_lib.models.JokerPopularity
 
 class JokerPopularityRepository(
     private val jokerPopularityDao: JokerPopularityDao
 ) {
 
-    suspend fun add(jokerPopularity: JokerPopularity) {
+    suspend fun addJP(jokerPopularity: JokerPopularity) {
         jokerPopularityDao.add(jokerPopularity = jokerPopularity)
     }
 
-    suspend fun get(): JokerPopularity? {
+    suspend fun getJP(): JokerPopularity? {
         return jokerPopularityDao.get()
     }
 
+    companion object {
+        fun build(context: Context): JokerPopularityRepository {
+            return Room.databaseBuilder(context, JokerPopularityDatabase::class.java, JP_DB_NAME)
+                .fallbackToDestructiveMigration()
+                .build()
+                .jokerPopularityDao()
+                .let { dao ->
+                    JokerPopularityRepository(dao)
+                }
+        }
+    }
 }
